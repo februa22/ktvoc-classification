713ecddeef10dbf33436692439ec8c93a66fa5b2
Jongseong Woo <jongseong.w@nhnent.com>
2018-11-14 15:55:23 +0900

    add ktvoc_noun72k_shuffled.sh

---
+++ ktvoc_noun72k_shuffled.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+USR_DIR=ktvoc
+PROBLEM=ktvoc_noun72k_shuffled
+MODEL=transformer
+HPARAMS=transformer_base
+
+DATA_DIR=/data/t2t_data/$PROBLEM
+TMP_DIR=/data/t2t_datagen
+TRAIN_DIR=/data/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+
+mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
+
+# Generate data
+# * The data is stored in a file named `ktvoc_noun72k_shuffled.pairs`
+t2t-datagen \
+  --t2t_usr_dir=$USR_DIR \
+  --data_dir=$DATA_DIR \
+  --tmp_dir=$TMP_DIR \
+  --problem=$PROBLEM
+
+# Train
+# *  If you run out of memory, add --hparams='batch_size=1024'.
+WORKER_GPU=3  # The number of GPUs for training
+export CUDA_VISIBLE_DEVICES=0,1,2  # GPU IDs to be used
+
+t2t-trainer \
+  --t2t_usr_dir=$USR_DIR \
+  --data_dir=$DATA_DIR \
+  --problem=$PROBLEM \
+  --model=$MODEL \
+  --hparams_set=$HPARAMS \
+  --worker_gpu=$WORKER_GPU \
+  --output_dir=$TRAIN_DIR \
+  --hparams='batch_size=2048'
